135cf30fdfbe96268aa3fff67b13ee9080af9f3e
neil-jay <neiljay@pcmeder.online>
2024-12-03 13:24:35 +0530

    fix: Fix sequential order

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -36,11 +36,11 @@ const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h5
-    ref={ref}
-    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
-    {...props}
-  />
+<h2
+  ref={ref}
+  className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+  {...props}
+/>
 ))
 AlertTitle.displayName = "AlertTitle"
 
@@ -57,4 +57,3 @@ const AlertDescription = React.forwardRef<
 AlertDescription.displayName = "AlertDescription"
 
 export { Alert, AlertTitle, AlertDescription }
-
